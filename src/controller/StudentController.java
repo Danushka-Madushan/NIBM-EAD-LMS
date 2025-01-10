3dbc617ee8e33362b47790ab0b4017c8a8597da0
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-01-10 16:41:58 +0530

    feat: create StudentController.java

---
+++ src/controller/StudentController.java
@@ -0,0 +1,44 @@
+package controller;
+
+
+import javax.swing.JOptionPane;
+import view.LoginView;
+import view.StudentView;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gh
+ */
+public class StudentController {
+    private StudentView view;
+
+    public StudentController(StudentView view) {
+        this.view = view;
+
+        view.addViewAttendanceListener(e -> viewAttendance());
+        view.addPayFeesListener(e -> payFees());
+        view.addLogoutListener(e -> logout());
+    }
+
+    private void viewAttendance() {
+        JOptionPane.showMessageDialog(view, "Viewing Attendance...");
+    }
+
+    private void payFees() {
+        JOptionPane.showMessageDialog(view, "Paying Fees...");
+    }
+
+    private void viewSchedule() {
+        JOptionPane.showMessageDialog(view, "Viewing Class Schedule...");
+    }
+
+    private void logout() {
+        view.dispose();
+        new LoginView().setVisible(true);
+    }
+}
